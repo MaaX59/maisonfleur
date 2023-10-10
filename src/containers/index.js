ded54c2b4668504fa41fc016f3780356e7a43519
MaaX59 <maax.strandberg@gmail.com>
2023-10-10 17:59:36 +0200

    more set up

---
+++ src/containers/index.js
@@ -0,0 +1,12 @@
+import Navbar from './Navbar/Navbar';
+import Header from './Header/Header';
+import Contact from './Contact/Contact';
+
+
+export {
+    Navbar, 
+    Header,
+    
+    Contact,
+    
+};
