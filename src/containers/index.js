def6ea3acee3ef599e403db7fac06f38794c09fc
MaaX59 <maax.strandberg@gmail.com>
2023-10-11 18:57:23 +0200

    more set up

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,12 +1,18 @@
 import Navbar from './Navbar/Navbar';
 import Header from './Header/Header';
 import Contact from './Contact/Contact';
+import Services from './Services/Services';
+import Gallery from './Gallery/Gallery';
+import About from './About/About';
+
 
 
 export {
     Navbar, 
     Header,
-    
+    Gallery,
+    Services,
+    About,
     Contact,
     
 };
