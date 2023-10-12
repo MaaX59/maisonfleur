9933bbfe2010fc78aa100a4dbdee1f38538e563d
MaaX59 <maax.strandberg@gmail.com>
2023-10-12 12:24:08 +0200

    navbar npm menu

--- src/containers/Navbar/Navbar.jsx
+++ src/containers/Navbar/Navbar.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import {React, useState} from 'react'
 import './Navbar.css'
+import Bouquet from '../../images/rose.png'
+import Hamburger from 'hamburger-react'
+
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false)
   return (
     <div className='app__navbar'>
       <div className='app__navbar-logo'>
+      <img src={Bouquet} alt="bouquet" />
       Maison De La Fleur</div>
+      <div className='app__navbar-menu'>
+        <Hamburger />
+      </div>
     </div>
   )
 }
