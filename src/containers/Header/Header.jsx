bc9e74c79ab0cd6987d65b2e0db9d11b06b26577
MaaX59 <maax.strandberg@gmail.com>
2023-10-14 17:08:07 +0200

    header

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './Header.css'
+import Bouquet from '../../images/flowers.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='app__header'>
+      <div className='app__header-text'>
+        <h1>Exclusive High-Style</h1>
+        <h2>Floral arrangements</h2>
+        <button> Shop Now</button>
+      </div>
+      <div className='app__header-img'>
+        <img src={Bouquet} alt="bouquet" />
+      </div>
+    </div>
   )
 }
 
