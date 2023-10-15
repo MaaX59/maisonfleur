816250d91d287ad601a22cd1aaddf959cc9b1859
MaaX59 <maax.strandberg@gmail.com>
2023-10-15 14:25:32 +0200

    services set up

--- src/containers/Services/Services.jsx
+++ src/containers/Services/Services.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import './Services.css'
-
+import Valentine from '../../images/valentine.jpg';
+import Mother from '../../images/mothersday.webp'
+import Wedding from '../../images/wedding.jpg'
 const Services = () => {
   return (
     <div className='app__services'>
-      <div className='app__services-weddings'></div>
-      <div className='app__services-valentine'></div>
+      <div className='app__services-weddings'>
+      <img src={Wedding} alt="wedding" />
+      <div className='app__services-weddings-content'></div>
+      </div>
+      <div className='app__services-valentine'>
+      <div className='app__services-valentine-content'></div>
+        <img src={Valentine} alt="valentine" />
+      </div>
+      <div className='app__services-mother'>
+        <img src={Mother} alt="mothers day" />
+        <div className='app__services-mother-content'></div>
+      </div>
       {/* <div className='app__services-'></div>
       <div className='app__services'></div> */}
     </div>
