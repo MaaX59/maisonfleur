e44f9f215caca2a1ebcc0afec23ca9e36b22116e
MaaX59 <maax.strandberg@gmail.com>
2023-10-16 18:48:06 +0200

    gallery kinda works

--- src/images/gallery/images.js
+++ src/images/gallery/images.js
@@ -6,12 +6,14 @@ import image5 from './images/gallery/5.webp';
 import image6 from './images/gallery/6.webp';
 import image7 from './images/gallery/7.jpg';
 import image8 from './images/gallery/8.jpg';
-import image9 from './images/gallery/9.jpg';
-import image10 from './images/gallery/10.jpg';
-import image11 from './images/gallery/11.png';
-import image12 from './images/gallery/12.jpg';
+// import {image9} from './images/gallery/9.jpg';
+// import {image10} from './images/gallery/10.jpg';
+// import {image11} from './images/gallery/11.png';
+// import {image12} from './images/gallery/12.jpg';
+import {bla} from './floral_frame.png'
 
 export default {
+    
     image1,
     image2,
     image3,
@@ -20,9 +22,10 @@ export default {
     image6,
     image7,
     image8,
-    image9,
-    image10,
-    image11,
-    image12,
+    // image9,
+    // image10,
+    // image11,
+    // image12,
+
 
 };
