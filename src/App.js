b44a19fb3ad837e592eab31bb6da4bf3a2752e1a
MaaX59 <maax.strandberg@gmail.com>
2023-10-17 12:35:58 +0200

    some small changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import {Navbar, About, Contact, Gallery, Header, Services, Review } from './containers/index';
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  Navbar,
+  About,
+  Contact,
+  Gallery,
+  Header,
+  Services,
+  Review,
+} from "./containers/index";
 
 function App() {
   return (
     <div className="app">
-    <Navbar />
-    <Header />
-    <Services />
-    <Review />
-    <Gallery />
-    
-    
-    <About />
-    <Contact />
+      <Navbar />
+      <Header />
+      <Services />
+      <Review />
+      <Gallery />
 
-      
+      <About />
+      <Contact />
     </div>
   );
 }
--- src/containers/Gallery/Gallery.jsx
+++ src/containers/Gallery/Gallery.jsx
@@ -1,28 +1,29 @@
-import {React, useRef} from "react";
-import {
-  BsArrowLeftShort,
-  BsArrowRightShort,
-} from "react-icons/bs";
-import './Gallery.css'
-import image1 from '../../images/gallery/1.webp';
-import image2 from '../../images/gallery/2.jpg';
-import image3 from '../../images/gallery/3.webp';
-import image4 from '../../images/gallery/4.webp';
-import image5 from '../../images/gallery/5.webp';
-import image6 from '../../images/gallery/6.webp';
-import image7 from '../../images/gallery/7.jpg';
-import image8 from '../../images/gallery/8.jpg';
-// // import {image9} from '../../images/gallery/9.jpg';
-// // import {image10} from '../../images/gallery/10.jpg';
-// // import {image11} from '../../images/gallery/11.png';
-// // import {image12} from '../../images/gallery/12.jpg';
+import { React, useRef } from "react";
+import { BsArrowLeftShort, BsArrowRightShort } from "react-icons/bs";
 
+import "./Gallery.css";
 
-const galleryImages = [image1, image2, image3, image4, image5, image6, image7, image8 ]
+import image1 from "../../images/gallery/1.webp";
+import image2 from "../../images/gallery/2.jpg";
+import image3 from "../../images/gallery/3.webp";
+import image4 from "../../images/gallery/4.webp";
+import image5 from "../../images/gallery/5.webp";
+import image6 from "../../images/gallery/6.webp";
+import image7 from "../../images/gallery/7.jpg";
+import image8 from "../../images/gallery/8.jpg";
 
+const galleryImages = [
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+];
 
 const Gallery = () => {
-console.log(galleryImages)
   const scrollRef = useRef(null);
 
   const scroll = (direction) => {
@@ -35,28 +36,31 @@ console.log(galleryImages)
   };
 
   return (
-    <div className="app__gallery app__bg" >
-    <div className="app__gallery-images">
-    <div className="app__gallery-images_container" ref={scrollRef}>
-      {galleryImages.map((image, index)=> (
-        <div className="app__gallery-images_card" key={`gallery_image- ${index + 1}`} > 
-        
-        <img src={image} alt="gallery" />
+    <div className="app__gallery app__bg">
+      <div className="app__gallery-images">
+        <div className="app__gallery-images_container" ref={scrollRef}>
+          {galleryImages.map((image, index) => (
+            <div
+              className="app__gallery-images_card"
+              key={`gallery_image- ${index + 1}`}
+            >
+              <img src={image} alt="gallery" />
+            </div>
+          ))}
         </div>
-      ))}
+        <div className="app__gallery-images_arrows">
+          <BsArrowLeftShort
+            className="gallery__arrow-icon"
+            onClick={() => scroll("left")}
+          />
+          <BsArrowRightShort
+            className="gallery__arrow-icon"
+            onClick={() => scroll("right")}
+          />
+        </div>
+      </div>
     </div>
-    <div className="app__gallery-images_arrows">
-      <BsArrowLeftShort
-        className="gallery__arrow-icon"
-        onClick={() => scroll("left")}
-      />
-      <BsArrowRightShort
-        className="gallery__arrow-icon"
-        onClick={() => scroll("right")}
-      />
-    </div>
-  </div></div>
-  )
-}
+  );
+};
 
-export default Gallery
+export default Gallery;
