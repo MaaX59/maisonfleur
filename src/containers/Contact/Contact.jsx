d8207079dcc32373b0ce4a408dd489eac7a362c0
MaaX59 <Maax.strandberg@gmail.com>
2023-10-27 21:25:12 +0200

    contact started

--- src/containers/Contact/Contact.jsx
+++ src/containers/Contact/Contact.jsx
@@ -3,7 +3,16 @@ import './Contact.css'
 
 const Contact = () => {
     return (
-      <div>Contact</div>
+      <div className='app__contact' id='contact'>
+        <div className='app__contact-title'>Contact Us</div>
+        <div className='app__contact-content'>
+          <div className='app__contact-content-email'>Email</div>
+          <div className='app__contact-content-hours'>Opening Hours</div>
+          <div className='app__contact-content-phone'>Phone Number</div>
+
+        </div>
+
+      </div>
     )
   }
   
